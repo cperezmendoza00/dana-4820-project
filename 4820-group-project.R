e33c5a16c3e592a7a26a70e54e5d95bf76f30b65
cadukeson <cadukeson@gmail.com>
2022-11-20 03:57:26 +0000

    R file and dataset

---
+++ 4820-group-project.R
@@ -0,0 +1,10 @@
+
+#rm(list=ls()); #Cleaning variables
+#detach(); #Detaching datasets
+#getwd() # Getting current directory
+#setwd("/yourfolder") # Setting work directory (it should be git folder)
+
+file = paste(getwd(),"/data/Loan-Approval-Prediction.csv", sep = "")
+DS = read.csv(file, sep = ";") 
+names(DS)
+
